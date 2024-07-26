4bca2bc87ac314f5c4c90333af87f3bba433f406
antosnizzah <antonygatitu327@gmail.com>
2024-07-26 21:06:11 +0300

    added the payment  page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,38 +1,53 @@
-
 import Slider from 'react-slick';
 import { cars } from '../data/cars';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-
 const About = () => {
   const settings = {
-    dots: true ,
+    dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
-    return (
-      <div className="container mx-auto p-4">
-        <h1 className="text-4xl font-bold text-center mb-8">About Our Cars</h1>
-        <Slider {...settings}>
-          {cars.map((car, index) => (
-            <div key={index} className="p-4">
-              <div className="bg-slate-50 rounded-lg shadow-lg overflow-hidden h-65">
-                <img src={car.image} alt={car.name} className="w-full h-60 object-cover" />
-                <div className="p-4">
-                  <h2 className="text-xl font-bold">{car.name}</h2>
-                  <p className="mt-2 text-gray-600">{car.specifications}</p>
-                </div>
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold text-center mb-8">About Our Cars</h1>
+      <Slider {...settings}>
+        {cars.map((car, index) => (
+          <div key={index} className="p-4">
+            <div className="bg-slate-50 rounded-lg shadow-lg overflow-hidden h-65">
+              <img src={car.image} alt={car.name} className="w-full h-60 object-cover" />
+              <div className="p-4">
+                <h2 className="text-xl font-bold">{car.name}</h2>
+                <p className="mt-2 text-gray-600">{car.specifications}</p>
               </div>
             </div>
-          ))}
-        </Slider>
-      </div>
-    );
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 };
 
 export default About;
